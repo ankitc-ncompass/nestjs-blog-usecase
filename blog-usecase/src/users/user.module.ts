e0b6e5a960b74e78e846b7e87afa1408b81c5480
arpita-ncompass <arpita@ncompass.inc>
2024-02-20 09:58:15 +0530

    minor changes in user module

--- blog-usecase/src/users/user.module.ts
+++ blog-usecase/src/users/user.module.ts
@@ -4,13 +4,25 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { User } from './entity/user.entity';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),
-    JwtModule.register({ secret: 'secret', signOptions: { expiresIn: '1d' } }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: { expiresIn: configService.get('EXPIRES_IN') },
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot(),
   ],
   controllers: [UserController],
   providers: [UserService],
+
 })
 export class UserModule {}
+
+ 
